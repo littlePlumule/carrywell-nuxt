4087d26b97dd2fc6b59d54500aa5649d4978b2dd
hung <ff789456741@gmail.com>
2026-03-02 18:21:38 +0800

    add: add google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,19 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/scss/style.scss'],
-  modules: ['@nuxt/icon'],
+  modules: ['@nuxt/icon', '@nuxtjs/google-fonts'],
   alias: {
     images: fileURLToPath(new URL('./app/assets/images', import.meta.url)),
+  },
+  googleFonts: {
+    families: {
+      Inter: [400, 500, 600, 700],
+      'Noto Serif TC': [400, 500, 700],
+      'Shippori Mincho': [400, 500, 700],
+      Agbalumo: [400, 500, 700],
+      Brawler: [400, 500, 700],
+    },
+    display: 'swap',
+    preload: true,
   },
 })
