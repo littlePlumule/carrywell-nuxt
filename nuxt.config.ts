e8dd26e139c61fda5d22bd7291675e4d73a60d6e
hung <ff789456741@gmail.com>
2026-03-03 17:40:28 +0800

    fix: vercel setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/scss/style.scss'],
   modules: ['@nuxt/icon', '@nuxtjs/google-fonts', '@vee-validate/nuxt'],
   googleFonts: {
